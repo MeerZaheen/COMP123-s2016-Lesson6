ef63007a7c414e8455916f6731ca50a7909e886d
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-06-17 11:49:19 -0400

    updated abstract GameObject class and created Vector2 class

--- PLayerProject/Program.cs
+++ PLayerProject/Program.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+* Author: 
+* Date
+* StudentID
+* Description: This program tests the player and enemy classes
+* Version: 0.01 - Initial Test Version - testing basic classes and methods
+*/
+
 namespace PLayerProject
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Player greenPlayer = new PLayerProject.Player("green");
+            greenPlayer.MoveForward();
+            WaitForAnyKey();
+            TouchingEnemy redEnemy = new TouchingEnemy("red");
+            redEnemy.MoveForward();
+        }
+
+        /**
+        * <summary>
+        * Utility method to wait for a console key press from the user
+        * </summary>
+        *
+        * @method WaitForAnyKey
+        * @returns {void}
+        */
+        public static void WaitForAnyKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("+++++++++++++++++++++");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
     }
 }
---
+++ PLayerProject/iShootable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PLayerProject
+{
+    public interface iShootable
+    {
+        void Shoot();
+    }
+}
